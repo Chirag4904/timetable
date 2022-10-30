bee4fdb023cf57c788079f686426f6dd83cc8b43
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-10-30 22:41:00 +0530

    define alotment schema

---
+++ server/src/models/allotment.mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// TODO: add min/max limits for the hours
+const allotmentSchema = Schema({
+    subject: {
+        type: Schema.Types.ObjectId,
+        ref: "Subject",
+        required: true,
+    },
+    allotedTeachers: [
+        new Schema({
+            teacher: {
+                type: Schema.Types.ObjectId,
+                ref: "teacher",
+                required: true,
+            },
+            lectureHrs: {
+                type: Number,
+                required: true,
+            },
+            tutorialHrs: {
+                type: Number,
+                required: true,
+            },
+            practicalHrs: {
+                type: Number,
+                required: true,
+            },
+        }),
+    ],
+});
+
+
+module.exports = mongoose.model("Allotment", allotmentSchema)
