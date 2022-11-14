5d9b2d9a538298c7887e57dcda188974f5fd03d9
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-14 11:16:47 +0530

    Subject state change feature added to backend Fix #37

--- server/src/routes/subjects/subjects.controller.js
+++ server/src/routes/subjects/subjects.controller.js
@@ -1,4 +1,7 @@
-const { getAllSubjects } = require("../../models/subjects.model");
+const {
+	getAllSubjects,
+	updateSubjectState,
+} = require("../../models/subjects.model");
 
 async function httpGetAllSubjects(req, res) {
 	const query = req.query;
@@ -6,6 +9,17 @@ async function httpGetAllSubjects(req, res) {
 	return res.send(subjects);
 }
 
+async function httpUpdateSubjectState(req, res) {
+	const subject = req.body;
+	//validation
+	if (!subject.id && !subject.isAssigned) {
+		return res.status(400).send("Invalid request");
+	}
+	await updateSubjectState(subject.id, subject.isAssigned);
+	return res.send("ok");
+}
+
 module.exports = {
 	httpGetAllSubjects,
+	httpUpdateSubjectState,
 };
