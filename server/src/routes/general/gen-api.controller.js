cf215ebcad3296dd1fae1264642d40a054af7d94
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-15 12:05:54 +0530

    total hours calculation

--- server/src/routes/general/gen-api.controller.js
+++ server/src/routes/general/gen-api.controller.js
@@ -33,6 +33,11 @@ async function commitLTPHandler(req) {
 
     // retrieve an allotment document, if it exists otherwis we create our own here
     const subject = await Subject.findOne({ id: req.subject_id.toUpperCase() });
+    let lecHours = subject.originalStructure.get("L") * subject.lectureBatches;
+    let tutHours = subject.originalStructure.get("T") * subject.tutLabBatches;
+    let labHours = subject.originalStructure.get("P") * subject.tutLabBatches;
+    let total = lecHours + tutHours + labHours;
+    console.log("total hours", total)
     const teach = await teacher.findOne({ id: req.teacher_id });
 
     if (!subject) {
@@ -48,6 +53,8 @@ async function commitLTPHandler(req) {
             subject: subject._id,
         });
     }
+
+
     // ==========================================================
 
     // DEBUG ONLY, TODO: remove this
