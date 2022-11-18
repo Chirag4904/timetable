48549846ff1bc57ec41958f5e47a8ecd77edd02c
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-19 02:00:50 +0530

    Add teacher to subject Frontend Fix #89

---
+++ client/svelte-timetable/src/components/Teacher Components/AddTeacher.svelte
@@ -0,0 +1,61 @@
+<script>
+    import { onMount } from "svelte";
+    import axios from "axios";
+    const teachersUrl = "http://localhost:5000/api/teachers";
+    const subjectsUrl = "http://localhost:5000/api/subjects";
+
+    export let subjectId;
+    let teachers;
+
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    //This is dispatching or firing the evend teacherAdded whenever add button is clicked
+    //This lets us know when the add button is clicked which is captured by the parent component
+    function dispatchTeacherAdded() {
+        dispatch("teacherAdded");
+    }
+
+    onMount(async function () {
+        await httpGetTeachers();
+    });
+    let selectedTeacher;
+    async function httpGetTeachers() {
+        const resp = await fetch(`${teachersUrl}`);
+
+        const data = await resp.json();
+        teachers = data;
+        selectedTeacher = teachers[0].id;
+        // console.log(teachers);
+    }
+
+    async function handleAddTeacher() {
+        try {
+            const resp = await axios.post(`${subjectsUrl}/addTeacher`, {
+                subjectId: subjectId,
+                teacherId: selectedTeacher,
+            });
+            dispatchTeacherAdded();
+            console.log(resp);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+</script>
+
+{#if teachers}
+    <div class="flex">
+        <div>
+            <select bind:value={selectedTeacher} placeholder="Select Teacher">
+                {#each teachers as teacher}
+                    <option value={teacher.id}>
+                        {teacher.name}
+                    </option>
+                {/each}
+            </select>
+        </div>
+
+        <button on:click={handleAddTeacher} class="cursor-pointer bg-red-200">
+            Add
+        </button>
+    </div>
+{/if}
